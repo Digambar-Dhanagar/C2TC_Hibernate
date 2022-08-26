d92b51b3fa992c6b8b419ffd99fcbf2a82599985
Digamber <Digamber@LAPTOP-0S7NUT3G>
2022-08-26 12:41:00 +0530

    first commit

--- Hibernate_JPA_CRUD_op/src/com/tns/jpacrud/service/StudentServiceImpl.java
+++ Hibernate_JPA_CRUD_op/src/com/tns/jpacrud/service/StudentServiceImpl.java
@@ -1,1 +1,42 @@
-first commit
+package com.tns.jpacrud.service;
+import com.jpacrud.dao.StudentDao;
+import com.jpacrud.dao.StudentDaoImpl;
+import com.tns.jpacrud.entities.Student;
+
+
+public class StudentServiceImpl implements StudentService {
+	 private StudentDao studentdao;
+		
+		public StudentServiceImpl()
+		{
+			studentdao=new StudentDaoImpl();
+		}
+		@Override
+		public Student findStudentById(int id) {
+			Student student=studentdao.getStudentById(id);
+			return student;
+		}
+
+		@Override
+		public void addStudent(Student student) {
+			studentdao.beginTransaction();
+			studentdao.addStudent(student);
+			studentdao.commitTransaction();	
+			
+		}
+
+		@Override
+		public void removeStudent(Student student) {
+			studentdao.beginTransaction();
+			studentdao.removeStudent(student);
+			studentdao.commitTransaction();	
+		}
+
+		@Override
+		public void updateStudent(Student student) {
+			studentdao.beginTransaction();
+			studentdao.updateStudent(student);
+			studentdao.commitTransaction();	
+		}
+
+}
